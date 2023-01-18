60211b78a1c71605d46f49ea8a83661a97e95fa3
Christoph Werner <christoph.werner@cosuno.de>
2023-01-18 09:03:23 +0100

    chore: upgrade to webpack 5 builder

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,11 @@
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.output.hashFunction = 'xxhash64';
+    return config;
+  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js', '../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-storysource', '@storybook/addon-a11y'],
 };
