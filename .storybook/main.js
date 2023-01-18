a745d4237bf11e9f9b821ae0d7d98aa9fcf0a874
Christoph Werner <christoph.werner@cosuno.de>
2023-01-18 09:08:44 +0100

    chore: upgrade node version

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,6 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
-  webpackFinal: async (config) => {
-    config.output.hashFunction = 'xxhash64';
-    return config;
-  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js', '../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-storysource', '@storybook/addon-a11y'],
 };
